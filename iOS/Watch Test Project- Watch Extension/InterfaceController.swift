855ee95079e92c154800db2573a5e58171ff22be
ebpark9511 <ebpark9511@gmail.com>
2020-10-26 17:23:00 +0900

    Setting Watch Project

---
+++ iOS/Watch Test Project- Watch Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  Watch Test Project- Watch Extension
+//
+//  Created by 박은비 on 2020/10/26.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
---
+++ iOS/Watch Test Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Watch Test Project
+//
+//  Created by 박은비 on 2020/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
