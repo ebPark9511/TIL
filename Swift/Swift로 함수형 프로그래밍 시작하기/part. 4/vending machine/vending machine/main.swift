fc9fd4bbeacc6f7b5c74fc18832225b543c7c253
ebpark9511 <ebpark9511@gmail.com>
2020-11-15 23:48:08 +0900

    201115

--- Swift/Swift로 함수형 프로그래밍 시작하기/part. 4/vending machine/vending machine/main.swift
+++ Swift/Swift로 함수형 프로그래밍 시작하기/part. 4/vending machine/vending machine/main.swift
@@ -16,167 +16,157 @@ import Foundation
  그래서 입력값은 사용자로부터 명령어를 입력받아 처리합니다.
  출력값도 콘솔출력으로 처리합니다.
  
- 100
- 현재 금액은 100 입니다.
- 500
- 현재 금액은 600 입니다.
- cola
- 잔액이 부족합니다.
- 500
- 현재 금액은 1100 입니다.
- cola
- 콜라(1000원) 상품이 나왔습니다.
- 현재 금액은 100 입니다.
- reset
- 잔액 100원이 나왔습니다.
- 현재 금액은 0 입니다.
- 
  */
 
-//Error 클래스 정의
-// 1. 잘못된 코인을 넣었을때.
-// 2. 없는 메뉴를 골랐을때.
-enum VendingMachineError: Error {
-    case wrongCoin
-    case notMenu
-}
 
- 
-enum CoinState: Int {
-    case success = 0
-    case fail
-}
- 
-enum CurrentState: Int {
-    case put = 0
-    case sell
-    case reset
-    case change
-    case lack
-}
- 
- 
 open class VendingMachine {
     
-    // 자판기 예약어
-    enum VendingMachineReservedWords {
-        case putCoin(coin: Int)
-        case putMenu(menu: String)
+    /*
+     자판기 기능
+     1. 동전을 먹는다
+     2. 물건을 준다
+     3. 잔돈을 준다.
+   */
+      
+    private enum VendingMachineError: Error {
+        case wrongCoin
+        case notMenu
+    }
+    
+    private enum VendingMachineReservedWords {
+        case getCoin(coin: Int)
+        case offerProduct(menu: String)
         case reset
     }
     
-    var menu: Dictionary<String, Int>
-    var coin: Int = 0
-    var state: CurrentState = .reset {
+    private enum CurrentState {
+        case balance(_ coin: Int)
+        case offer(menu: String, coin: Int)
+        case reset(coin: Int)
+        case lack
+    }
+
+    typealias Product = (String, Int)
+    
+    private var menu: Dictionary<String, Int>
+    private var coin: Int = 0
+    private var state: CurrentState = .reset(coin: 0) {
         willSet { printState(newValue) }
     }
-    
-  
-    
-      
-    
+     
     init(menu: Dictionary<String, Int>) {
         self.menu = menu
     }
       
-    func put(_ word: VendingMachineReservedWords) throws {
-  
-        switch word {
-        case .putCoin(coin: let coin):
-            guard coin == 100 || coin == 500 || coin == 1000 else {
-                throw VendingMachineError.wrongCoin
-            }
-            self.coin += coin
-            printState(.put)
-            
-        case .putMenu(menu: let menu):
-            guard self.menu.contains(where: { $0.key == menu }) else {
-                throw VendingMachineError.notMenu
-            }
-            
-        case .reset:
-            print("reset")
-  
-        }
-        
-  
+    private func resetCoin() {
+        coin = 0
+        printState(.balance(coin))
     }
     
-    func getMenu(_ menu: String) {
+    private func getMenu(_ menu: String) throws -> Product {
         if let price = self.menu[menu] {
-           
-            if self.coin < price {
-                printState(.lack)
-                return
-            }
-            
-            self.coin = self.coin - price
-            printState(.sell)
-            return
-        }
-        
-        if menu == "reset" {
-            self.coin = 0
-            printState(.reset)
+            return (menu, price)
         }
          
-        return // 오류
-    }
-    
-    func printState(_ state: CurrentState) {
-        switch state {
-        case .put:
-            print("현재 금액은 \(coin) 입니다.")
-        case .sell:
-            print("콜라(1000원) 상품이 나왔습니다.")
-        case .change:
-            print("잔액 100원이 나왔습니다.")
-            printState(.put)
-        case .lack:
-            print("잔액이 부족합니다.")
-        default:
-            return
-        }
+        throw VendingMachineError.notMenu 
     }
      
-}
-
-
-/*
- 콜라 : 1000
- 사이다 : 1100
- 환타 : 1200
- */
-
-
-let drinkVendingMachine = VendingMachine(menu: ["cola": 1000, "cider": 1100, "Fanta": 1200] )
-
-// 사용자 입장에서는
-// 코인을 넣고, 상품을 선택하고, 잔돈을 받는 일을 해야함.
-
-while true {
-    if let read = readLine(){
-        
+    private func printState(_ state: CurrentState) {
+        switch state {
+        case .balance(coin : let coin):
+            print("현재 금액은 \(coin) 입니다.")
+        case .offer(menu: let menu, coin: let coin):
+            print("\(menu)(\(coin)원) 상품이 나왔습니다.")
+        case .lack:
+            print("잔액이 부족합니다.")
+        case .reset(coin: let coin):
+            print("잔액 \(coin)원이 나왔습니다.")
+        }
+    }
+    
+    
+    private func operateVendingMachine(_ word: VendingMachineReservedWords) {
         do {
             
-            // 하기부분도 자판기 책임.
-            // input된 데이터를 check하여 putCoin과 putReservedWords로 나눔
-            if let coin = Int(read) {
-                try drinkVendingMachine.put(.putCoin(coin: coin))
-            } else {
-                if read == "reset" {
-                    try drinkVendingMachine.put(.reset)
-                } else {
-                    try drinkVendingMachine.put(.putMenu(menu: read))
+            switch word {
+            case .getCoin(coin: let coin):
+                guard coin == 100 || coin == 500 || coin == 1000 else {
+                    throw VendingMachineError.wrongCoin
                 }
+                self.coin += coin
+                printState(.balance(self.coin))
+                
+            case .offerProduct(menu: let menu):
+                let product = try getMenu(menu)
+                
+                if coin < product.1 {
+                    printState(.lack)
+                    return
+                }
+                
+                coin = coin - product.1
+                printState(.offer(menu: product.0, coin: product.1))
+                
+            case .reset:
+                printState(.reset(coin: coin))
+                resetCoin()
             }
             
         } catch VendingMachineError.wrongCoin {
-            print("잘못된 코인.")
+            print("not defined coin")
         } catch VendingMachineError.notMenu {
-            print("존재하지 않는 메뉴")
+            print("doesn't have menu")
+        } catch {
+            print("error")
+        }
+    }
+    
+    func put(_ words: UserWords) {
+        switch words {
+        case .putCoin(coin: let coin):
+            operateVendingMachine(.getCoin(coin: coin))
+        case .putMenu(menu: let menu):
+            operateVendingMachine(.offerProduct(menu: menu))
+        case .reset:
+            operateVendingMachine(.reset)
         }
         
+    }
+    
+}
+
+
+ 
+let drinkVendingMachine = VendingMachine(menu: ["cola": 1000,
+                                                "cider": 1100,
+                                                "Fanta": 1200] )
+
+/* 사용자
+ 1. 코인을 넣는다.
+ 2. 상품을 선택한다.
+ 3. 잔돈을 받는다. */
+
+
+
+enum UserWords {
+    case putCoin(coin: Int)
+    case putMenu(menu: String)
+    case reset
+}
+ 
+while true {
+    
+    if let read = readLine(){
         
+        if let coin = Int(read) {
+            drinkVendingMachine.put(.putCoin(coin: coin))
+        } else {
+            if read == "reset" {
+                drinkVendingMachine.put(.reset)
+                continue
+            }
+             
+            drinkVendingMachine.put(.putMenu(menu: read))
+        }
     }
 }
